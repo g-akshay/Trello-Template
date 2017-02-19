9b6ae829b8e62bb2a723948f2044214cc772b60e
G Akshay <gakshayapps@gmail.com>
2017-02-19 16:54:01 +0530

    initial commit

---
+++ js/custom/main.js
@@ -0,0 +1,30 @@
+$.utilities = (function() {
+
+    // ajax utils
+    var _fetchData;
+
+    return utilities = {
+
+    };
+
+}());
+
+$(document).ready(function() {
+   $(".content").mCustomScrollbar({
+        axis: "x", 
+        theme:"dark",
+        scrollbarPosition: "inside",
+        setWidth: '100%',
+        mouseWheel:{ scrollAmount: 300 }
+    });
+
+   /*$('.list-item').mCustomScrollbar({
+        axis: "y", 
+        theme:"dark",
+        scrollbarPosition: "inside",
+        setWidth: '100%',
+        mouseWheel:{ scrollAmount: 300 }
+    });*/
+
+});
+
